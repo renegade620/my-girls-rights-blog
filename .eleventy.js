6ed339c6a262cc4a363f5d78db34ea82ea61fbad
Frankline Were <franklinegift@gmail.com>
2025-06-16 16:19:08 +0300

    modify template

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,23 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/uploads");
-  eleventyConfig.addPassthroughCopy("static/admin");
+  eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("_posts/*.md").reverse();
   });
+
+  // Add a date filter for Nunjucks
+  eleventyConfig.addNunjucksFilter("readableDate", function(dateObj) {
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+  });
+
   return {
     dir: {
       input: ".",
       includes: "_includes",
-      data: "_data",
       output: "_site"
     }
   };
